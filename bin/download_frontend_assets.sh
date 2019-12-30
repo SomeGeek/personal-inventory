fbdc77c17defc3f49d0221bb8da1bc6f4ec3bfef
Sean McGrath <sean@mcgrath.net.nz>
2019-12-30 19:41:45 +1300

    Update JS versions

--- bin/download_frontend_assets.sh
+++ bin/download_frontend_assets.sh
@@ -9,9 +9,9 @@ vendor_download() {
 
 (
     cd $BASEDIR &&
-    vendor_download https://stackpath.bootstrapcdn.com/bootswatch/4.1.2/lumen/bootstrap.min.css bootstrap.min.css &&
-    vendor_download https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js bootstrap.min.js &&
-    vendor_download https://code.jquery.com/jquery-3.3.1.min.js jquery.min.js &&
-    vendor_download https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css select2.min.css &&
-    vendor_download https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js select2.min.js
+    vendor_download https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/lumen/bootstrap.min.css bootstrap.min.css &&
+    vendor_download https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js bootstrap.min.js &&
+    vendor_download https://code.jquery.com/jquery-3.4.1.min.js jquery.min.js &&
+    vendor_download https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css select2.min.css &&
+    vendor_download https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js select2.min.js
 )
